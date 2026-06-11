3b8a40f8730534171e9551e409b8061fb3000217
mhubsources <mahdidarzi1024@gmail.com>
2026-06-11 12:07:34 +0330

    chore: Exeptiopn exampple has been implemented

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Person> workers = new ArrayList<>();
-        workers.add(new Manager("Mahdi", 26, "land", 12));
-        workers.add(new Employee("Mahdi", 26,"test"));
-        for (Person person:workers) {
-            person.showInfo();
-            person.showRole();
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
         }
+        System.out.println("Program continues...");
     }
 }
