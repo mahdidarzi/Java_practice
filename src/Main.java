2718707af04348a185e7684c59da75cb3946cefa
mhubsources <mahdidarzi1024@gmail.com>
2026-06-11 15:52:21 +0330

    chore: FileWriter exampple has been implemented

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-     Bank b1=new Bank(100);
         try {
-            b1.withdraw(9);
-        } catch (InsufficientFundsException e) {
-            throw new RuntimeException(e);
+            FileWriter writer=new FileWriter("data.txt");
+            writer.write("Hello Java File!\n");
+            writer.write("This is line 2.\n");
+            writer.write("This is line 3.\n");
+            writer.close();
+
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing file.");
         } finally {
         }
     }
