f0fbe26b310e595af53fe138c3eaf6d5c3138dc1
mhubsources <mahdidarzi1024@gmail.com>
2026-06-13 21:18:01 +0330

    chore: project structure  has been updated

--- StudentManagementSystem/src/exception/InvalidStudentDataException.java
+++
@@ -1,5 +0,0 @@
-public class InvalidStudentDataException extends Exception{
-    public InvalidStudentDataException(String message) {
-        super(message);
-    }
-}
--- StudentManagementSystem/src/repository/IStudentRepository.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface IStudentRepository {
-    List<Student> findAll();
-    void saveAll(List<Student> students);
-}
